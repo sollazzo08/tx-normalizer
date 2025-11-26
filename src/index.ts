11b278f5aa84fdf5ddb00deed1865a6258e1f562
Sollazzo08 <msollazzo08@gmail.com>
2025-11-26 01:46:47 -0500

    support frontend only

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-// importTDCsv is for frontend apps and importTdCsvFromFile is for cli or backend
-export { importTdCsv, importTdCsvFromFile } from "./adapters/td";
-
+export { importTdCsv } from "./adapters/td";
